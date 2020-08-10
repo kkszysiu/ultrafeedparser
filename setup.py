39899b24ef0bf385af06ff74d1bbe1be24a20f0e
Krzysztof Klinikowski <kkszysiu@gmail.com>
2020-08-10 08:10:35 +0700

    Update sources

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup
+
+try:
+    from setuptools_rust import RustExtension, Binding
+except ImportError:
+    import subprocess
+
+    errno = subprocess.call([sys.executable, "-m", "pip", "install", "setuptools-rust"])
+    if errno:
+        print("Please install setuptools-rust package")
+        raise SystemExit(errno)
+    else:
+        from setuptools_rust import RustExtension
+
+setup_requires = ["setuptools-rust==0.10.3", "wheel"]
+install_requires = []
+
+setup(
+    name="ultrafeedparser",
+    version="0.1.0",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    packages=["ultrafeedparser"],
+    rust_extensions=[RustExtension("ultrafeedparser.libultrafeedparser", binding=Binding.RustCPython)],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ ultrafeedparser/__init__.py
@@ -0,0 +1,1 @@
+from ultrafeedparser.libultrafeedparser import *
