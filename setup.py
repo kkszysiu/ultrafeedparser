23058f25d2a4355b5c076fcb368ab8c811444c3a
Krzysztof Klinikowski <kkszysiu@gmail.com>
2022-12-03 16:20:52 +0100

    Update libs, update version to 1.0.1

--- setup.py
+++ setup.py
@@ -8,19 +8,19 @@ try:
 except ImportError:
     import subprocess
 
-    errno = subprocess.call([sys.executable, "-m", "pip", "install", "setuptools-rust==0.10.3"])
+    errno = subprocess.call([sys.executable, "-m", "pip", "install", "setuptools-rust==1.5.2"])
     if errno:
         print("Please install setuptools-rust package")
         raise SystemExit(errno)
     else:
         from setuptools_rust import RustExtension
 
-setup_requires = ["setuptools-rust==0.10.3", "wheel"]
+setup_requires = ["setuptools-rust==1.5.2", "wheel"]
 install_requires = []
 
 setup(
     name="ultrafeedparser",
-    version="0.1.0",
+    version="0.1.1",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
